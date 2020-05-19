5cdf38b252ffad261418bac4a3f4d673bfc9a2c2
j8seangel <jseangel@gmail.com>
2020-05-19 19:28:28 +0200

    migrate to typescript

--- index.ts
+++ index.ts
@@ -14,39 +14,42 @@ const BASE_SVG_PATH_TEMPLATE =
 // Well if Bhutan is supported I think we're good to go
 const supportsEmojiFlags = ifEmoji('🇧🇹')
 
-const countryflag = (iso) => {
-  let iso2
-  let ISO2
+const countryflag = (iso: string) => {
+  let iso2: string
+  let ISO2: string
   let country
   if (iso.length === 2) {
     iso2 = iso.toLowerCase()
     ISO2 = iso2.toUpperCase()
     country = countries.find((c) => c.ISO2 === ISO2)
     if (country === undefined) {
-      throw new Error('Could not find a country corresponding to provided ISO:', iso)
+      throw new Error(`Could not find a country corresponding to provided ISO: ${iso}`)
     }
-
   } else if (iso.length === 3) {
     const ISO3 = iso.toUpperCase()
     country = countries.find((c) => c.ISO3 === ISO3)
     if (country === undefined) {
-      throw new Error('Could not find a country corresponding to provided ISO:', iso)
+      throw new Error(`Could not find a country corresponding to provided ISO: ${iso}`)
     }
     ISO2 = country.ISO2
     iso2 = ISO2.toLowerCase()
   } else {
-    throw new Error(iso, ' is not an ISO 2 nor ISO 3 code')
+    throw new Error(`${iso} is not an ISO 2 nor ISO 3 code`)
   }
 
   let emoji = null
 
-  if (supportsEmojiFlags) {
-    emoji = String.fromCodePoint(
-      // maps a capital letter A with the character 🇦
-      // both letters combined should return an emoji
-      ISO2.codePointAt(0) + CODEPOINT_OFFSET,
-      ISO2.codePointAt(1) + CODEPOINT_OFFSET
-    )
+  if (supportsEmojiFlags && ISO2) {
+    const codePoint0 = ISO2!.codePointAt(0)
+    const codePoint1 = ISO2!.codePointAt(1)
+    if (codePoint0 && codePoint1) {
+      emoji = String.fromCodePoint(
+        // maps a capital letter A with the character 🇦
+        // both letters combined should return an emoji
+        codePoint0 + CODEPOINT_OFFSET,
+        codePoint1 + CODEPOINT_OFFSET
+      )
+    }
   }
 
   const svg = BASE_SVG_PATH_TEMPLATE.replace('{iso2}', iso2)
